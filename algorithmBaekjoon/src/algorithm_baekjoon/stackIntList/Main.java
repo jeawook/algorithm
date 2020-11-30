a0f48c889506fb4566ab85c4e2dd8d33782cf669
jeawook <pjw4825@naver.com>
2020-11-30 21:11:56 +0900

    백준 9012 번 괄호
    백준 9012번 괄호

---
+++ algorithmBaekjoon/src/algorithm_baekjoon/stackIntList/Main.java
@@ -0,0 +1,22 @@
+package algorithm_baekjoon.stackIntList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int num = Integer.parseInt(br.readLine());
+        int[] list = new int[num];
+        int ptr = 0;
+        int stv = 1;
+        for (int i=0;i<num;i++){
+            int value = Integer.parseInt(br.readLine());
+
+        }
+        System.out.println(sb.toString());
+    }
+}
