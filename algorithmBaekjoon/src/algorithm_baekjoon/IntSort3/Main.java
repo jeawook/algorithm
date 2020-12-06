966404fc06b802e7882c71540d1fa18dd89a1345
jeawook <pjw4825@naver.com>
2020-12-06 21:47:45 +0900

    10989번 수 정렬하기3
    10989번 수 정렬하기3

---
+++ algorithmBaekjoon/src/algorithm_baekjoon/IntSort3/Main.java
@@ -0,0 +1,27 @@
+package algorithm_baekjoon.IntSort3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 10989번 수 정렬하기3
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int num = Integer.parseInt(br.readLine());
+        int[] counting = new int[10001];
+        for(int i=0;i<num;i++){
+            counting[Integer.parseInt(br.readLine())]++;
+        }
+
+        for (int i=0;i< counting.length;i++){
+            for (int j=0;j<counting[i];j++){
+                sb.append(i+"\n");
+            }
+        }
+        System.out.println(sb.toString());
+
+
+    }
+}
