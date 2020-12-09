d7e601256d323c90c6682dcfe8ca9974a4ea6e4c
jeawook <pjw4825@naver.com>
2020-12-09 16:06:41 +0900

    11720번 숫자의 합
    11720번 숫자의 합

---
+++ algorithmBaekjoon/src/algorithm_baekjoon/intSum/Main.java
@@ -0,0 +1,19 @@
+package algorithm_baekjoon.intSum;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+// 11720번 숫자의 합
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] list = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
+        int sum = 0;
+        for (int x : list)
+            sum+=x;
+        System.out.println(sum);
+    }
+}
