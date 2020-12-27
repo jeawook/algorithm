08d3addd35def21860c69ab357895213859e159b
jeawook <pjw4825@naver.com>
2020-12-27 16:21:52 +0900

    11654번 아스키 코드
    11654번 아스키 코드

---
+++ algorithmBaekjoon/src/algorithm_baekjoon/ASCII/Main.java
@@ -0,0 +1,16 @@
+package algorithm_baekjoon.ASCII;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//11654번 아스키 코드
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String ch = br.readLine();
+        System.out.println(ch.charAt(0) - 0);
+
+    }
+}
