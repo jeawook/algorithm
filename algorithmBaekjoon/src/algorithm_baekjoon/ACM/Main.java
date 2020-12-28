c89c76d68c4a21e15cadd8f7e2c9f51a5a9b3521
jeawook <pjw4825@naver.com>
2020-12-28 23:25:16 +0900

    10250번 ACM 호텔
    10250번 ACM 호텔

---
+++ algorithmBaekjoon/src/algorithm_baekjoon/ACM/Main.java
@@ -0,0 +1,28 @@
+package algorithm_baekjoon.ACM;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//10250번 ACM 호텔
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int T =  Integer.parseInt(br.readLine());
+
+        for (int i=0;i<T;i++) {
+            st = new StringTokenizer(br.readLine());
+            int H = Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+            int room = N % H == 0 ? N/H  : N/H+1;
+            int floor = N%H == 0 ? H : N%H;
+            floor *= 100;
+            System.out.println(room+floor);
+        }
+    }
+
+}
