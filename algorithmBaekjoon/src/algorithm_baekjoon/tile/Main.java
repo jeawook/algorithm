96af8c0676d68df8d7acdc6c1f1399723ebc42cb
jeawook <pjw4825@naver.com>
2021-01-01 14:53:51 +0900

    1904번 01타일
    1904번 01타일

--- algorithmBaekjoon/src/algorithm_baekjoon/tile/Main.java
+++ algorithmBaekjoon/src/algorithm_baekjoon/tile/Main.java
@@ -1,45 +1,24 @@
 package algorithm_baekjoon.tile;
 
 import java.io.*;
-//3
-//100
-//001
-//111
 
-//4
-// 5
-//5
-// 8
+
 //1904번 01타일
-//6
-//000000
-//000011
-//001100
-//110000
-//100001
-//001001
-//100100
-//111100
-//110011
-//001111
-//111111
-//100111
-//111001
 
 public class Main {
+    static Integer[] dp = new Integer[1000001];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int x = Integer.parseInt(br.readLine());
-        int[] d = new int[x+3];
-
-        d[0] = 0;
-        d[1] = 1;
-        d[2] = 2;
-        for (int i = 3; i <=x; i++) {
-            d[i] = d[i-1] + d[i-2];
-            d[i] = d[i] %15746;
+        int N = Integer.parseInt(br.readLine());
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        System.out.println(recur(N));
+    }
+    static int recur(int N) {
+        if (dp[N] == null) {
+            dp[N] = (recur(N-1)+recur(N-2))%15746;
         }
-        System.out.print(d[x]);
-
+        return dp[N];
     }
 }
