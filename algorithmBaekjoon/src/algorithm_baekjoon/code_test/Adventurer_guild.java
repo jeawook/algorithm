e9be1247a5ad623a5232a4efbf13a56f28125298
jeawook <pjw4825@naver.com>
2021-01-10 20:24:58 +0900

    Update Adventurer_guild.java

--- algorithmBaekjoon/src/algorithm_baekjoon/code_test/Adventurer_guild.java
+++ algorithmBaekjoon/src/algorithm_baekjoon/code_test/Adventurer_guild.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class adventurer_guild {
+public class Adventurer_guild {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
