0e2d0f4b58791f1cba41f55732b28f223673943f
jeawook <pjw4825@naver.com>
2021-01-14 15:58:18 +0900

    greedy 체육복
    greedy 체육복

---
+++ algorithm_progremers/src/algorithm_progremers/workout_clothes/solution.java
@@ -0,0 +1,42 @@
+package algorithm_progremers.workout_clothes;
+
+//greedy 체육복
+public class solution {
+    /**
+     *
+     * @param n = 5
+     * @param lost {2,4}
+     * @param reserve {1, 3, 5}
+     * @return 5
+     */
+
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n;
+        int cnt = lost.length;
+        int[] check = new int[n+2];
+        for(int i : reserve) {
+            check[i] = 2;
+        }
+        for(int i : lost) {
+            if(check[i] == 2){
+                cnt--;
+            }
+            check[i]--;
+        }
+
+        for(int i : lost) {
+            if(check[i] == -1){
+                if(check[i-1] == 2) {
+                    check[i-1]--;
+                    cnt--;
+                }else if(check[i+1] == 2) {
+                    check[i+1]--;
+                    cnt--;
+                }
+            }
+
+        }
+
+        return answer-cnt;
+    }
+}
