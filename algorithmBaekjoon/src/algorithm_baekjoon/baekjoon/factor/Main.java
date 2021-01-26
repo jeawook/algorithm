6124f1fba7239551b736c39ec48bcbe24dacd9fb
jeawook <pjw4825@naver.com>
2021-01-26 22:00:03 +0900

    1037번 약수
    1037번 약수

---
+++ algorithmBaekjoon/src/algorithm_baekjoon/baekjoon/factor/Main.java
@@ -0,0 +1,24 @@
+package algorithm_baekjoon.baekjoon.factor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+//1037번 약수
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[0] * arr[N-1]);
+
+    }
+}
