9f43a2fe6e817fab912b2310651778dadccdaf1a
jeawook <pjw4825@naver.com>
2021-03-31 22:43:45 +0900

    Update Solution.java

--- algorithm_progremers/src/algorithm_progremers/compaction/Solution.java
+++ algorithm_progremers/src/algorithm_progremers/compaction/Solution.java
@@ -7,14 +7,16 @@ import java.util.Queue;
 
 public class Solution {
     public static void main(String[] args) {
-        String msg = "KAKAO";
+        //String msg = "KAKAO";
+        String msg = "ABABABABABABABAB";
+        //String msg = "TOBEORNOTTOBEORTOBEORNOT";
         Solution solution = new Solution();
         solution.solution(msg);
     }
 
     public int[] solution(String msg) {
         LinkedList<Integer> arr = new LinkedList<>();
-        String[] strings = {"A","B","C","D","E","F","G","H","I","J","K","L","N","M","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+        String[] strings = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
         HashMap<String, Integer> map = new HashMap<>();
         for (int i = 0; i < strings.length; i++) {
             map.put(strings[i], i+1);
@@ -22,28 +24,28 @@ public class Solution {
         }
 
         for (int i = 0; i < msg.length(); i++) {
-            int ptr = 1;
-            int location = -1;
-            while (location != 0 && i + ptr < msg.length()) {
-                ptr++;
-                location = map.getOrDefault(msg.substring(i, i+ptr), 0);
-                System.out.println("location = " + location);
-            }
-            
-            String str = msg.substring(i, i+ptr);
-            System.out.println("str = " + str);
-            int t = map.get(str);
-            arr.add(t);
-            if (ptr > 2) {
-                i++;
+
+            for(int j= 0; i+j < msg.length(); j++) {
+                String tmp = msg.substring(i, j + i + 1);
+                int length = msg.length();
+                int location = map.getOrDefault(tmp, 0);
+                if (location == 0 ) {
+                    arr.add(map.get(msg.substring(i,j+i)));
+                    map.put(tmp, map.size()+1);
+                    i += j-1;
+                    break;
+                } else if (i+j == length-1) {
+                    i += j;
+                    arr.add(map.get(tmp));
+                    break;
+                }
             }
         }
-        System.out.println("arr.size() = " + arr.size());
         int[] answer = new int[arr.size()];
-        for (int i = 0; i < arr.size(); i++) {
+        for (int i = 0; i < answer.length; i++) {
             answer[i] = arr.remove();
-            System.out.println("answer = " + answer[i]);
         }
+
         return answer;
     }
 }
