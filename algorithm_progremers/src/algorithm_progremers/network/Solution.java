a104fcd270bf8d312869e0e4e96887cc32508569
jeawook <pjw4825@naver.com>
2021-06-27 16:37:21 +0900

    Create Solution.java

---
+++ algorithm_progremers/src/algorithm_progremers/network/Solution.java
@@ -0,0 +1,14 @@
+package algorithm_progremers.network;
+
+public class Solution {
+    public void main(String[] args) {
+        int n = 3;
+        int[][] computers = {{1,1,0}, {1,1,0}, {0,0,1}};
+    }
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+
+        return answer;
+    }
+    
+}
