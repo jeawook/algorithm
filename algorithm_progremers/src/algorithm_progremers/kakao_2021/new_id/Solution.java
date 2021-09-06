0f6aa9b82e4a9df5c2ff1d08c4773b36b4354785
jeawook <pjw4825@naver.com>
2021-09-06 23:38:05 +0900

    Create Solution.java

---
+++ algorithm_progremers/src/algorithm_progremers/kakao_2021/new_id/Solution.java
@@ -0,0 +1,17 @@
+package algorithm_progremers.kakao_2021.new_id;
+
+class Solution {
+
+    public static void main(String[] args) {
+
+        String new_id = "...!@BaT#*..y.abcdefghijklm";
+        String result = "bat.y.abcdefghi";
+        result.equals(solution(new_id));
+
+    }
+
+    public static String solution(String new_id) {
+        String answer = "";
+        return answer;
+    }
+}
